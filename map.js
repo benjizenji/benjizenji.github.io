d0b322048780f5c8939d213c020f4e2aa51d2946
Benji <bfs14b@my.fsu.edu>
2015-09-12 21:23:05 -0400

    receive data and plot

--- map.js
+++ map.js
@@ -1,34 +1,34 @@
+$(document).ready(function() {
 
-function initialize() 
+});
+
+$.getJSON('https://battletrip.herokuapp.com/misses', function(data) {
+	initialize(data.arr);
+});
+
+function initialize(data) 
 {
-var myOptions = {center: new google.maps.LatLng(42.2814, -83.7483),
- zoom: 12,
- mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
- 
-var coordinates = [{latitude:42.290895, longitude:-83.705983},
-	{latitude:42.267687, longitude:-83.714981},
-	{latitude:42.293469, longitude:-83.780727},
-	{latitude:42.267243, longitude:-83.750257},
-	{latitude:42.278230, longitude:-83.759012}
-	];
+	var myOptions = {center: new google.maps.LatLng(42.2814, -83.7483),
+	 zoom: 12,
+	 mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
 
-for(var i=0; i<coordinates.length; i++) {
-  var lat = coordinates[i].latitude;
-  var lon = coordinates[i].longitude;
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(lat, lon),
-    map: map
-  });
-}
+	// var coordinates = [{latitude:42.290895, longitude:-83.705983},
+	// 	{latitude:42.267687, longitude:-83.714981},
+	// 	{latitude:42.293469, longitude:-83.780727},
+	// 	{latitude:42.267243, longitude:-83.750257},
+	// 	{latitude:42.278230, longitude:-83.759012}
+	// 	];
+	var coordinates = data;
 
-// BackUp
-// var lat = 42.267243;
-// var lon = -83.750257;
-//   marker = new google.maps.Marker({
-//     position: new google.maps.LatLng(lat, lon),
-//     map: map
-// });
- 
+	for(var i=0; i<coordinates.length; i++) {
+	  var lat = coordinates[i].lat;
+	  var lon = coordinates[i].lon;
+	  marker = new google.maps.Marker({
+	    position: new google.maps.LatLng(lat, lon),
+	    map: map
+	  });
+	}
+	 
 }
