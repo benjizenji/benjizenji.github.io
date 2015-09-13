a7a8953a4cb920abccf4fa77826f1eea1d970914
Benji <bfs14b@my.fsu.edu>
2015-09-12 23:23:32 -0400

    circles!

--- map.js
+++ map.js
@@ -1,43 +1,49 @@
+var map;
 $(document).ready(function() {
-
-});
-
-/*
-$.getJSON('https://battletrip.herokuapp.com/misses', function(data) {
-	initialize(data.arr);
-}); */
-
-var url = 'http://battletrip.herokuapp.com/misses';
-$.ajax({
-  type: "GET",
-  url: url,
-  dataType: "jsonp",
-  contentType: "application/json",
-  success: function(result){
-  	//console.log(result);
-  	initialize(result.arr);
-  },
-  error: function(xhr, textStatus, errThrown){
-  	console.log(errThrown);
-  }
-});
-
-function initialize(data) 
-{
+	//load map
 	var myOptions = {center: new google.maps.LatLng(42.2814, -83.7483),
 	 zoom: 12,
 	 mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
-	var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+	map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
 
-	// var coordinates = [{latitude:42.290895, longitude:-83.705983},
-	// 	{latitude:42.267687, longitude:-83.714981},
-	// 	{latitude:42.293469, longitude:-83.780727},
-	// 	{latitude:42.267243, longitude:-83.750257},
-	// 	{latitude:42.278230, longitude:-83.759012}
-	// 	];
+	//matt's shit for misses
+	var url = 'http://battletrip.herokuapp.com/misses';
+	$.ajax({
+	  type: "GET",
+	  url: url,
+	  dataType: "jsonp",
+	  contentType: "application/json",
+	  success: function(result){
+	  	//console.log(result);
+	  	initialize(result.arr);
+	  },
+	  error: function(xhr, textStatus, errThrown){
+	  	console.log(errThrown);
+	  }
+	});
+
+	//shit for circles
+	var url = 'http://battletrip.herokuapp.com/ships';
+	$.ajax({
+	  type: "GET",
+	  url: url,
+	  dataType: "jsonp",
+	  contentType: "application/json",
+	  success: function(result){
+	  	//console.log(result);
+	  	drawCircles(result.arr);
+	  },
+	  error: function(xhr, textStatus, errThrown){
+	  	console.log(errThrown);
+	  }
+	});
+});
+
+function initialize(data) 
+{
 	var coordinates = data;
-
+	//plot flags
 	for(var i=0; i<coordinates.length; i++) {
 	  var lat = coordinates[i].lat;
 	  var lon = coordinates[i].lon;
@@ -45,6 +51,26 @@ function initialize(data)
 	    position: new google.maps.LatLng(lat, lon),
 	    map: map
 	  });
+	}	 
+}
+
+function drawCircles(data)
+{
+	var coordinates = data;
+	//plot circles
+	for(var i=0; i<coordinates.length; i++) {
+		var lat = coordinates[i].lat;
+	 	var lon = coordinates[i].lon;
+	 	var radius = coordinates[i].radius;
+		var circle = new google.maps.Circle({
+			strokeColor: '#FF0000',
+		    strokeOpacity: 0.5,
+		    strokeWeight: 1.5,
+		    fillColor: '#FF0000',
+		    fillOpacity: 0.35,
+	    	map: map,
+	    	center: {lat: lat, lng: lon},
+	    	radius: radius
+		});
 	}
-	 
 }
