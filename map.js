2010bce1f75aa81ef0104d995ff4821a28392cdd
Benji <bfs14b@my.fsu.edu>
2015-09-19 19:49:02 -0400

    bad id on div

--- map.js
+++ map.js
@@ -211,6 +211,7 @@ window.onresize = function(event) {
   var docWidth = $(document).width();
   var headWidth = docWidth - 20;
   var header = document.getElementById("header").offsetWidth = headWidth + "px";
-  var killheader = document.getElementById("header").style.width;
+  var killheader = document.getElementById("header");
+  killheader.style.width = header;
   console.log(killheader);
 }
