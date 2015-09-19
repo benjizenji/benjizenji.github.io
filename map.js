9df34d9b1381c624280c870638746429df5c4817
Benji <bfs14b@my.fsu.edu>
2015-09-19 19:32:41 -0400

    offset debugging

--- map.js
+++ map.js
@@ -210,6 +210,6 @@ function drawCircles(data)
 window.onresize = function(event) {
   var docWidth = $(document).width();
   var headWidth = docWidth - 20;
-  var header = document.getElementById("heading").offsetWidth = headWidth;
+  var header = document.getElementById("heading").offsetWidth;
   console.log(docWidth, headWidth, header);
 }
