4a9ed00f1f6891262f304042cdbea09b776337c3
Benji <bfs14b@my.fsu.edu>
2015-09-19 20:07:11 -0400

    bad id on div

--- map.js
+++ map.js
@@ -208,7 +208,7 @@ function drawCircles(data)
 }
 
 window.onresize = function(event) {
-  var docWidth = $(document).width();
+  var docWidth = $(document).innerWidth();
   var headWidth = docWidth - 20;
   var header = document.getElementById("header").offsetWidth = headWidth + "px";
   var killheader = document.getElementById("header");
